ed1a2aecd5fe3a597b6311837de3a2cfd1215208
Eduard Duarte <eduard.duarte@hotmail.com>
2019-08-01 16:10:26 -0500

    programa base

---
+++ src/app/app-routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent }
+];
