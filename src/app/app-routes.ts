dbd01a0a456e5cd97c7774b454976183b2776463
Eduard Duarte <eduard.duarte@hotmail.com>
2019-08-01 16:15:03 -0500

    programa base modif

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
 const appRoutes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent },
+  { path: 'progress', component: ProgressComponent },
+
+
 ];
