8c1b8e76752d5134a90bcc3f15a57187ec02a24d
Eduard Duarte <eduard.duarte@hotmail.com>
2019-08-01 17:51:53 -0500

    se agrega login

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,4 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
@@ -6,12 +7,19 @@ import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ]
+  },
+
   { path: 'register', component: LoginComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graficas1', component: Graficas1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NopagefoundComponent }
 ];
 export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
