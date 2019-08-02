75e4ae1d028f1826b60af14458dd1b425379acc7
Eduard <eduard.duarte@hotmail.com>
2019-08-01 22:59:26 -0500

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//Modulos
+import { PagessModule } from './pages/pagess.module';
 
-// Rutas
-import { APP_ROUTES } from "./app-routes";
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,30 +11,28 @@ import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { PagesComponent } from './pages/pages.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
+// Rutas
+import { APP_ROUTES } from "./app-routes";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
     HeaderComponent,
     SidebarComponent,
     BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent,
-
   ],
   imports: [
     BrowserModule,
+    PagessModule,
     APP_ROUTES
   ],
   providers: [],
