5c92ed0297a110714e75af6021d805f7e4bc867c
Eduard Duarte <eduard.duarte@hotmail.com>
2019-08-05 09:05:43 -0500

    clean module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,9 @@ import { ShareModule } from "./shared/share.module";
 import { ComponentsModule } from "./components/components.module"
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { RegisterComponent } from './login/register.component';
+import { LoginComponent } from './login/login.component';
+
 
 // Rutas
 import { APP_ROUTES } from "./app-routes";
