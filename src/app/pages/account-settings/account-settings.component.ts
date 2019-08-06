f32e318fa01529233a5d987b4465e839977b6cd8
Eduard Duarte <eduard.duarte@hotmail.com>
2019-08-06 17:33:37 -0500

    check account settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-account-settings',
@@ -9,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
       }
   `]
 })
-export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+@Injectable()
+export class AccountSettingsComponent {
+  themeTag: HTMLElement;
+  constructor(@Inject(DOCUMENT) private _document: Document) {
+    this.themeTag = this._document.getElementById('theme');
+  }
+
+  changeTheme(theme: string, ref) {
+    let url = `assets/css/colors/${theme}.css`;
+    this.themeTag.setAttribute("href", url);
+    // let themeName=`${theme}-theme`
+    this.cleanCheck();
+
+    //usando referencia #
+    ref.classList.add('working');
+
+    //sin referencia
+    // this._document.getElementsByClassName(theme + "-theme")[0].classList.add("working");
+  }
+
+  // Se puede realizar con jQuery o con JavaScript
+  //Recorre los elementos con de la clase selector y les elmina la clase "working"
+  cleanCheck() {
+    let selecHTMLElm = this._document.getElementsByClassName('selector');
+    // let selecHTMLElm = this._document.querySelectorAll('.selector');
+
+    for (const elem of selecHTMLElm as any) {
+      elem.classList.remove("working");
+    }
+  }
 
   ngOnInit() {
   }
